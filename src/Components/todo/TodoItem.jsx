aa14b557d0fe1f5272bbeeda8fee9354000c5a89
Rokuazery <Rokuro81@gmail.com>
2024-04-02 12:18:32 +0700

    Fixed animation duration and stuttering issue

--- src/Components/todo/TodoItem.jsx
+++ src/Components/todo/TodoItem.jsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 const TodoItem = ({index, task, deleteTodo, editTodo, toggleTodo}) => {
   return (
-    <div className='bg-gray-100 rounded-md p-3 flex justify-between items-center cursor-pointer  hover:bg-gray-200 hover:text-lg transition-all text-sm relative' onClick={() => toggleTodo(task.id)}>
+    <div className='bg-gray-100 rounded-md p-3 flex justify-between items-center cursor-pointer  hover:bg-gray-200 hover:text-lg transition-all text-sm relative duration-[350ms]' onClick={() => toggleTodo(task.id)}>
 
       <div className={(task.completed ? 'bg-green-400': 'bg-yellow-400') + ' px-[2px] h-full w-fit absolute left-0'}></div>
       <p className='ml-[2px]'>{task.task}</p>
       <div className='space-x-3'>
-        <FontAwesomeIcon className='text-gray-600 hover:text-black transition-all ease-in-out bg-white shadow-sm rounded-md p-2' icon={faEdit} onClick={(e) => {e.stopPropagation(); editTodo(task.id)}}></FontAwesomeIcon>
-        <FontAwesomeIcon className='text-gray-600 hover:text-black transition-all ease-in-out bg-white shadow-sm rounded-md p-2' icon={faTrash} onClick={(e) => {e.stopPropagation(); deleteTodo(task.id);}}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-gray-600 hover:text-black transition-all ease-in-out bg-white shadow-sm rounded-md p-2 text-sm' icon={faEdit} onClick={(e) => {e.stopPropagation(); editTodo(task.id)}}></FontAwesomeIcon>
+        <FontAwesomeIcon className='text-gray-600 hover:text-black transition-all ease-in-out bg-white shadow-sm rounded-md p-2 text-sm' icon={faTrash} onClick={(e) => {e.stopPropagation(); deleteTodo(task.id);}}></FontAwesomeIcon>
       </div>
   </div>
   )
